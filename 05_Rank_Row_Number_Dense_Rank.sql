c35a0c9fedcbe1e6aa663072075c8c7b9a56e8a9
Saumya Singh <131527869+ssing2517@users.noreply.github.com>
2025-06-03 11:49:09 +0530

    Update 05_Rank_Row_Number_Dense_Rank.sql

--- 05_Rank_Row_Number_Dense_Rank.sql
+++ 05_Rank_Row_Number_Dense_Rank.sql
@@ -4,11 +4,10 @@
 -- DENSE_RANK(): Assigns a rank to each row
 -- Does NOT skip ranks when there are duplicates
 
-select *,rank() over( order by salary DESC) as rnk from employee
-
-select *,DENSE_RANK() over( order by salary DESC) as rnk from employee
-
-select *,ROW_NUMBER() over( order by salary DESC) as rnk from employee
+select *,ROW_NUMBER() over( order by salary DESC) as rn,
+RANK() over( order by salary DESC) as rnk ,
+DENSE_RANK() over( order by salary DESC) as d_rnk 
+from employee
 
 
 
