1a5ba984514ea910b283b482d79d029f1a7872f6
Saumya Singh <131527869+ssing2517@users.noreply.github.com>
2025-06-25 11:52:11 +0530

    Update 04_Union_vs_Union_All.sql

--- 04_Union_vs_Union_All.sql
+++ 04_Union_vs_Union_All.sql
@@ -8,3 +8,19 @@ SELECT * from employee;
 select * from employee_1
 union
 SELECT * from employee;
+--Query the two cities in STATION with the shortest and longest CITY names, as well as their respective lengths
+-- (i.e.: number of characters in the name). 
+-- If there is more than one smallest or largest city, choose the one that comes first when ordered alphabetically.
+(
+    SELECT CITY, LENGTH(CITY) AS name_length
+    FROM STATION
+    ORDER BY LENGTH(CITY), CITY
+    LIMIT 1
+)
+UNION
+(
+    SELECT CITY, LENGTH(CITY) AS name_length
+    FROM STATION
+    ORDER BY LENGTH(CITY) DESC, CITY
+    LIMIT 1
+);
