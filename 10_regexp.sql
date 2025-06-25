aa7a1f9d86f6d76f823f8771c783e14d3051adfa
Saumya Singh <131527869+ssing2517@users.noreply.github.com>
2025-06-25 12:28:10 +0530

    Update 10_regexp.sql

--- 10_regexp.sql
+++ 10_regexp.sql
@@ -17,3 +17,27 @@ WHERE CITY REGEXP '^[aeiouAEIOU].*[aeiouAEIOU]$';
 -- [aeiouAEIOU] ensures the first character is a vowel.
 -- .* allows for any characters in the middle.
 -- [aeiouAEIOU]$ makes sure the last character is also a vowel.
+
+-- Query the list of CITY names from STATION that do not start with vowels. Your result cannot contain duplicates.
+SELECT DISTINCT CITY 
+FROM STATION 
+WHERE CITY NOT REGEXP '^[aeiouAEIOU]';
+
+-- Query the list of CITY names from STATION that do not end with vowels. Your result cannot contain duplicates.
+SELECT DISTINCT CITY 
+FROM STATION 
+WHERE CITY NOT REGEXP '[aeiouAEIOU]$';
+
+--Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels. 
+SELECT DISTINCT CITY
+FROM STATION
+WHERE CITY NOT REGEXP '^[aeiouAEIOU]'
+  OR CITY NOT REGEXP '[aeiouAEIOU]$';
+
+
+--Query the list of CITY names from STATION that do not start with vowels and do not end with vowels.
+
+SELECT DISTINCT CITY
+FROM STATION
+WHERE CITY NOT REGEXP '^[aeiouAEIOU]'
+  AND CITY NOT REGEXP '[aeiouAEIOU]$';
