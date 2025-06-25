176538bb1defc9f9252acd6efbc3973742cd264e
Saumya Singh <131527869+ssing2517@users.noreply.github.com>
2025-06-25 12:07:15 +0530

    Update 10_regexp.sql

--- 10_regexp.sql
+++ 10_regexp.sql
@@ -7,3 +7,13 @@ WHERE CITY REGEXP '[aeiouAEIOU]$';
 SELECT DISTINCT CITY
 FROM STATION
 WHERE CITY REGEXP '^[aeiouAEIOU]';
+
+---starts and ends with vowel
+SELECT DISTINCT CITY
+FROM STATION
+WHERE CITY REGEXP '^[aeiouAEIOU].*[aeiouAEIOU]$';
+
+-- ^ asserts the start of the string.
+-- [aeiouAEIOU] ensures the first character is a vowel.
+-- .* allows for any characters in the middle.
+-- [aeiouAEIOU]$ makes sure the last character is also a vowel.
